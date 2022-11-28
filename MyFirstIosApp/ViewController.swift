28ad57eabd3b5c31c6686ffd192953dfb302f414
Osman Nuri Yıldız <iamosmannyildiz@gmail.com>
2022-11-28 13:07:33 +0300

    Initial commit

---
+++ MyFirstIosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstIosApp
+//
+//  Created by Osman Nuri Yıldız on 28.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
